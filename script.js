8e8f32fdabfe664082e92114b12a66c92236e1a6
Duan Lee-Watkins <leeduan19@gmail.com>
2020-11-18 17:16:28 -0500

    updated index.html and script.js

--- script.js
+++ script.js
@@ -4,47 +4,30 @@ const apiKey = "RcvgwyJ";
 function getStrains(query) {
   const url = searchURL + apiKey + "/strains/search/race/" + query;
   let strain = "";
-  let strainDesc = "";
-
-  console.log(url);
-
   fetch(url)
     .then((res) => res.json())
-    .then((strains) => {
+    .then(async (strains) => {
       let html = "";
-
-      for (let i = 0; i < strains.length; i++) {
-        strain = strains[i];
-
-        html += `
+      for (let i = 0; i < 20; i++) {
+        const urlTwo =
+          searchURL + apiKey + "/strains/data/desc/" + strains[i].id;
+        let desc = await fetch(urlTwo);
+        desc.json().then((data) => {
+          html += `
             <div class="col-6 col-with-margins">
                 <div class="card" data-strain-id="1">
                     <div class="card-body">
-                        <h3 class="card-title h4">${strain.name}</h3>
-                        <p class="card-text" style="display:block"></p>
+                        <h3 class="card-title h4">${strains[i].name}</h3>
+                        <p class="card-text" style="display:block">${data.desc}</p>
                     </div>
                 </div>
             </div>
           `;
-
-        const urlTwo = searchURL + apiKey + "/strains/data/desc/" + strain.id;
-
-        console.log(urlTwo);
-
-        fetch(urlTwo)
-          .then((res) => res.json())
-          .then((json) => {
-            // - [ ] Get the description from the response
-            // - [ ] Render the description in the card element
-
-            $(`.card[data-strain-id=${strain.id}] .card-text(html)`);
-          });
+        });
       }
-
       $("#strain-cards").html(html);
     });
 }
-
 function watchForm() {
   $("form").submit((event) => {
     event.preventDefault();
