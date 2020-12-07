55aed03dab8b878d63e5c8b917512869f010b82f
Duan Lee-Watkins <leeduan19@gmail.com>
2020-12-07 18:14:09 -0500

    Cleaned up pagination, strain description, and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# juana-Finder
+# Juana Finder
+
+- Live Site https://duanleewatkins.github.io/juana-Finder/
+  ![](image/juanaFinderPagePreview.png)
+
+If you are a newbie to the Marijuana world or a Marijauna connoisseur, [Juana Finder] (https://duanleewatkins.github.io/juana-Finder/) is the app for you. Juana Finder allows users to search for any strain of marijuana based on the three race types: Indica, Sativa, and Hybrid. Each strain also has a short discription of the strain detailing the user of it's origin, flavor, and effects.
+
+This app:
+-Utilizes a custom mobile first designs.
+-Uses JQuery, JavaScript, HTML, Bootstrap, and CSS.
+-Access data from http://strains.evanbusse.com/
--- script.js
+++ script.js
@@ -1,31 +1,71 @@
 "use strict";
 
-const itemsPerPage = 60;
-
 const searchURL = "https://strainapi.evanbusse.com/";
 const apiKey = "RcvgwyJ";
 
-function renderPagination({ totalPages, currentPage }) {
+// Pagination variables
+const ITEMS_PER_PAGE = 60;
+let CURRENT_PAGE = 2;
+let TOTAL_PAGES = 0;
+
+function renderStrainCard({ id, name }) {
+  return `
+    <div class="card" data-strain-id="${id}">
+        <div class="card-body">
+            <h3
+              class="card-title h4"
+              data-toggle="collapse"
+              data-target="#desc-${id}"
+              aria-expanded="false"
+              aria-controls="desc-${id}">
+              ${name}
+            </h3>
+            <div id="desc-${id}" class="collapse">
+              <p class="strain-desc"></p>
+            </div>
+        </div>
+    </div>
+  `;
+}
+
+function renderStrainCardList(strains) {
+  return strains
+    .map((strain) => {
+      return `
+        <div class="col-lg-3 col-md-4 col-sm-6 col-with-margins">
+          ${renderStrainCard(strain)}
+        </div>
+      `;
+    })
+    .join("");
+}
+
+function renderPagination({ currentPage, totalPages }) {
   let buttonsHTML = "";
 
-  for (let i = 1; i <= totalPages; i++) {
+  for (let i = 0; i < totalPages; i++) {
+    const disabledClass = i === currentPage ? "disabled" : "";
+
     buttonsHTML += `
-      <li class="page-item ${
-        i === currentPage + 1 ? "disabled" : ""
-      }"><a class="page-link" href="#">${i}</a></li>
+      <li class="page-item nav-page ${disabledClass}" data-page="${i}">
+        <a class="page-link" href="#">${i + 1}</a>
+      </li>
     `;
   }
 
+  const disabledPreviousClass = currentPage === 0 ? "disabled" : "";
+  const disabledNextClass = currentPage + 1 == totalPages ? "disabled" : "";
+
   const html = `
       <ul class="pagination justify-content-center">
-        <li class="page-item ${currentPage === 0 ? "disabled" : ""}">
+        <li id="nav-previous" class="page-item ${disabledPreviousClass}">
           <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
         </li>
+        
         ${buttonsHTML}
-        <li class="page-item ${
-          currentPage + 1 == totalPages ? "disabled" : ""
-        }">
-          <a class="page-link" href="#">Next</a>
+
+        <li id="nav-next" class="page-item ${disabledNextClass}">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Next</a>
         </li>
       </ul>
   `;
@@ -33,52 +73,82 @@ function renderPagination({ totalPages, currentPage }) {
   $("#pagination").html(html).removeClass("hidden");
 }
 
+function renderStrainPage({ currentPage, itemsPerPage, strains }) {
+  const totalPages = Math.ceil(strains.length / itemsPerPage);
+
+  const strainsToDisplay = strains.slice(
+    itemsPerPage * currentPage,
+    itemsPerPage * currentPage + itemsPerPage
+  );
+
+  const html = renderStrainCardList(strainsToDisplay);
+  $("#strain-cards").html(html);
+
+  // Fetch strain descriptions from API asynchronously and render in parallel
+  strainsToDisplay.forEach(getStrainDescription);
+
+  // Renders the strain pages based on the strains list length divided by the  items per page.
+  //If the totalPages is not a whole number, it will get rounded up using Math.ceil.
+  renderPagination({ totalPages, currentPage });
+
+  $(".nav-page").click((ev) => {
+    ev.preventDefault();
+
+    const requestedPage = $(ev.currentTarget).data("page");
+    renderStrainPage({ currentPage: requestedPage, itemsPerPage, strains });
+  });
+
+  $("#nav-previous").click((ev) => {
+    ev.preventDefault();
+    if (currentPage === 0) return;
+
+    const requestedPage = currentPage - 1;
+    renderStrainPage({ currentPage: requestedPage, itemsPerPage, strains });
+  });
+
+  $("#nav-next").click((ev) => {
+    ev.preventDefault();
+    if (currentPage === totalPages - 1) return;
+
+    const requestedPage = currentPage + 1;
+    renderStrainPage({ currentPage: requestedPage, itemsPerPage, strains });
+  });
+}
+
+function getStrainDescription(strain) {
+  return fetch(searchURL + apiKey + "/strains/data/desc/" + strain.id)
+    .then((response) => response.json())
+    .then((json) => {
+      const $card = $(`.card[data-strain-id=${strain.id}]`);
+
+      $(".strain-desc", $card).html(json.desc).show();
+    });
+}
+
 function getStrains(query) {
   fetch(searchURL + apiKey + "/strains/search/race/" + query)
     .then((res) => res.json())
     .then((strains) => {
-      let html = "";
-
-      strains.forEach((strain) => {
-        html += `
-            <div class="col-lg-3 col-md-4 col-sm-6 col-with-margins">
-                <div class="card" data-strain-id="${strain.id}">
-                    <div class="card-body">
-                        <h3 class="card-title h4"
-                          data-toggle="collapse"
-                          data-target="#desc-${strain.id}"
-                          aria-expanded="false"
-                          aria-controls="desc-${strain.id}">
-                            ${strain.name}
-                        </h3>
-                        <div id="desc-${strain.id}" class="collapse">
-                          <p class="strain-desc"></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-          `;
-      });
-
-      $("#strain-cards").html(html);
-      renderPagination({
-        totalPages: Math.ceil(strains.length / itemsPerPage),
+      renderStrainPage({
         currentPage: 0,
+        itemsPerPage: 60,
+        strains,
       });
 
-      strains.forEach((strain) => {
+      // Shows the descriptions for each strain.
+      /* strains.forEach((strain) => {
         fetch(searchURL + apiKey + "/strains/data/desc/" + strain.id)
           .then((response) => response.json())
           .then((json) => {
-            const $card = $(`.card[data-strain-id=${strain.id}]`);
-
-            if (!json.desc) {
+            const $card = $(`.card[data-strain-id=${strain.id}]`); */
+      // If a Strain does not have a description, remove the strain.
+      /* if (!json.desc) {
               return $card.parent().remove();
             }
 
             $(".strain-desc", $card).html(json.desc).show();
           });
-      });
+      }); */
     });
 
   $("#results").removeClass("hidden");
